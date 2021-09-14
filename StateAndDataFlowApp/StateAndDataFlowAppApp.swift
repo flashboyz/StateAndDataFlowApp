6de584b31bcbaafbc46240621bc51f5ced038a3c
Konstantin Prokofiev <flashboyz@yandex.ru>
2021-09-14 12:50:32 +0300

    Initial Commit

---
+++ StateAndDataFlowApp/StateAndDataFlowAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateAndDataFlowAppApp.swift
+//  StateAndDataFlowApp
+//
+//  Created by Константин Прокофьев on 14.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct StateAndDataFlowAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
